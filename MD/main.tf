f1295fc23afdebc3ff4b2b160bdfb712254180eb
aamer <aamer@Ameers-MacBook-Air.local>
2023-07-30 13:25:08 +0530

    Updated

--- MD/main.tf
+++ MD/main.tf
@@ -110,31 +110,6 @@ provider "helm" {
 data "aws_caller_identity" "current" {
 }
 
-# module "dns" {
-#   source                     = "./modules/api/external-dns"
-#   name_prefix                = "development"
-#   dns_hosted_zone            = "psinc.click"
-#   admin_users                = ["aamer"]
-#   developer_users            = ["mani", "anand"]
-#   external_dns_iam_role      = "external-dns"
-#   external_dns_chart_name    = "external-dns"
-#   external_dns_chart_repo    = "https://kubernetes-sigs.github.io/external-dns"
-#   external_dns_chart_version = "1.9.0"
-#   cluster_endpoint           = module.eks.cluster_endpoint
-
-#   external_dns_values = {
-#     "image.repository"   = "k8s.gcr.io/external-dns/external-dns",
-#     "image.tag"          = "v0.11.0",
-#     "logLevel"           = "info",
-#     "logFormat"          = "json",
-#     "triggerLoopOnEvent" = "true",
-#     "interval"           = "5m",
-#     "policy"             = "sync",
-#     "sources"            = "{ingress}"
-#   }
-
-# }
-
 module "argocd" {
   source = "./modules/api/argocd"
   values = [
